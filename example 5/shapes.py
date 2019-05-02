63c644ad875d6c4ada76a9a9600e3a78d3db6008
natmurei <47848668+natmurei@users.noreply.github.com>
2019-05-02 19:24:05 +0300

    added a new file

---
+++ example 5/shapes.py
@@ -0,0 +1,39 @@
+class Circle:
+	def __init__(self, r):
+		self.r = r
+
+	def a_circle(self,a):	
+		self.a = (22/7(r)*r)
+	def c_circle(self,c):	
+		self.c = (2*22/7)
+		
+class Squre:
+	def __init__(self,a):
+		self.a =a
+
+	def a_square(self,a):	
+		self.a = (a*a)
+	def p_square(self,p):	
+		self.p = (a*4)
+
+class Rectangle:
+	def __init__(self,w,l):
+		self.w = w
+		self.l = l
+
+	def a_rectangle(self,a):	
+		self.a = (w*l)
+	def p_rectangle(self,p):	
+		self.p = ((wl)*2)
+
+
+class Sphere:
+	def __init__(self,r ):
+		self.r = r
+	def a_sphere(self,a):	
+		self.a =(4*22/7(r)*r)
+	def	v_sphere(v):
+		self.v = (4/3*22/7(r)*9)
+		
+		
+
